330dd14333e62017f4c2bbccb9903ceca76e49ea
Felipe Pires Goncalves <felipe.goncalves@ibm.com>
2023-02-14 11:26:19 -0300

    Adcionando comentario na no arquivo URL

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+#URLS que serão passadas no browser
 urlpatterns = [
     path("", views.index, name="index"), # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
