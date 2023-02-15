4c104126b90e2b46d1669063419f496aa42b8cbb
Felipe Pires Goncalves <felipe.goncalves@ibm.com>
2023-02-15 10:06:53 -0300

    corrigindo CSS

--- challenges/views.py
+++ challenges/views.py
@@ -3,9 +3,18 @@ from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 monthly_challenges = {
-    "january": "Read 20 minutes a day",
-    "february": "Walk or training for 30 minutes",
-    "march": None
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": None
 }
 
 
